d24b19cb8e91c90045c5e7a52c8ca918ed815d76
Peter Eisenlohr <peter@eisenlohr.org>
2022-04-12 12:26:12 +0000

    LLVM 14

---
+++ .devcontainer/llvm-alternatives.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#
+#
+VERSION=14
+PRIORITY=100
+
+CMD=(update-alternatives --install /usr/lib/llvm llvm /usr/lib/llvm-$VERSION $PRIORITY)
+
+#
+# read all symlinks in /usr/bin that link to /usr/lib/llvm-*
+#
+while read FILE
+do
+    DEST=$(readlink -f "$FILE");
+    [[ $DEST == /usr/lib/llvm-$VERSION/* ]] || continue
+    [[ $FILE == *-$VERSION ]] || continue
+    TARGET=${FILE%*-$VERSION}
+    BASENAME=${TARGET##*/}
+    CMD+=(--slave "${FILE%*-$VERSION}" "$BASENAME" "$FILE")
+done < <(find /usr/bin -maxdepth 1 -type l)
+
+"${CMD[@]}"
